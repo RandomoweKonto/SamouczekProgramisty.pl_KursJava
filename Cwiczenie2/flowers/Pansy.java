27a2e17ddc43db3e6b600f46c67a4beb28a789c2
RandomoweKonto <110485309+RandomoweKonto@users.noreply.github.com>
2022-08-02 19:28:57 +0200

    Create Pansy.java

---
+++ Cwiczenie2/flowers/Pansy.java
@@ -0,0 +1,14 @@
+package pl.samouczekprogramisty.kursjava.flowers;
+
+public class Pansy {
+
+    private float noOfPatels;
+
+    public float getNoOfPatels(){
+        return noOfPatels;
+    }
+
+    public void setNoOfPatels(float noOfPatels){
+        this.noOfPatels = noOfPatels;
+    }
+}
