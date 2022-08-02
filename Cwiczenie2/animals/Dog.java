4fa6669033ae84ec0cf23cb151360a969244c33c
RandomoweKonto <110485309+RandomoweKonto@users.noreply.github.com>
2022-08-02 19:27:18 +0200

    Add files via upload

---
+++ Cwiczenie2/animals/Dog.java
@@ -0,0 +1,13 @@
+package pl.samouczekprogramisty.kursjava.animals;
+
+public class Dog {
+    private int barkiness;
+
+    public int getBarkiness(){
+        return barkiness;
+    }
+
+    public void setBarkiness(int barkiness){
+        this.barkiness = barkiness;
+    }
+}
---
+++ Cwiczenie2/animals/Horse.java
@@ -0,0 +1,14 @@
+package pl.samouczekprogramisty.kursjava.animals;
+
+public class Horse {
+
+    private short neighness;
+
+    public short getNeighness(){
+        return neighness;
+    }
+
+    public void setNeighness(short neighness){
+        this.neighness = neighness;
+    }
+}
