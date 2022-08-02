24d048e2a38a012ba5200c51d85b8d48f451bdf4
RandomoweKonto <110485309+RandomoweKonto@users.noreply.github.com>
2022-08-02 19:29:12 +0200

    Add files via upload

---
+++ Cwiczenie2/flowers/Rose.java
@@ -0,0 +1,14 @@
+package pl.samouczekprogramisty.kursjava.flowers;
+
+public class Rose {
+
+    private String colour;
+
+    public String getColour(){
+        return colour;
+    }
+
+    public void setColour(String colour){
+        this.colour = colour;
+    }
+}
---
+++ Cwiczenie2/flowers/Tulip.java
@@ -0,0 +1,14 @@
+package pl.samouczekprogramisty.kursjava.flowers;
+
+public class Tulip {
+
+    private byte height;
+
+    public byte getHeight(){
+        return height;
+    }
+
+    public void setHeight(byte height){
+        this.height = height;
+    }
+}
