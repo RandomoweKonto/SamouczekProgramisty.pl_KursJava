6ca26f31ad17ad645b4ae2b45a6c497e41756beb
RandomoweKonto <110485309+RandomoweKonto@users.noreply.github.com>
2022-08-02 19:27:04 +0200

    Create Cat.java

---
+++ Cwiczenie2/animals/Cat.java
@@ -0,0 +1,13 @@
+package pl.samouczekprogramisty.kursjava.animals;
+
+public class Cat {
+    private double purriness;
+
+    public double getPurriness(){
+        return purriness;
+    }
+
+    public void setPurriness(double purriness){
+        this.purriness = purriness;
+    }
+}
