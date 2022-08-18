76a06c06dc0937426151b84b006ca3a7c33cde3c
RandomoweKonto <110485309+RandomoweKonto@users.noreply.github.com>
2022-08-18 17:25:14 +0200

    Create Main.java

---
+++ cwiczenie9dziedziczenie/Main.java
@@ -0,0 +1,15 @@
+package pl.samouczekprogramisty.kursjava.cwiczenie9dziedziczenie;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal goldfish = new Goldfish();
+        Animal human = new Human();
+
+        System.out.println(goldfish);
+        System.out.println(human);
+
+    }
+
+}
