879363a383b4561b6eb88606fb495a35384c5d0b
RandomoweKonto <110485309+RandomoweKonto@users.noreply.github.com>
2022-08-18 17:23:32 +0200

    Create Human.java

---
+++ cwiczenie9dziedziczenie/Human.java
@@ -0,0 +1,13 @@
+package pl.samouczekprogramisty.kursjava.cwiczenie9dziedziczenie;
+
+public class Human extends Mammal{
+
+  @Override
+  public String toString(){
+
+      return "Hence, I am an idiot.";
+
+  }
+
+    }
+
