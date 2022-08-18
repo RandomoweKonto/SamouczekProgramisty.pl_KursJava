d8bccf5116028709284e671e1cc1b36c1802d84f
RandomoweKonto <110485309+RandomoweKonto@users.noreply.github.com>
2022-08-18 17:22:51 +0200

    Create Goldfish.java

---
+++ cwiczenie9dziedziczenie/Goldfish.java
@@ -0,0 +1,12 @@
+package pl.samouczekprogramisty.kursjava.cwiczenie9dziedziczenie;
+
+public class Goldfish extends Fish {
+
+    @Override
+    public String toString(){
+        return "I couldn't have done it without looking at your answer.";
+    }
+
+    }
+
+
